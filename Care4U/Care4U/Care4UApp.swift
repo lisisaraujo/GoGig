86297b7fb349525b3dbbbe217dc835fc65cb4b89
Lisis <laruschel@proton.me>
2024-10-29 13:57:43 +0100

    - login and registration works - home browse is possible without login, but user needs to login to go the main actions of the app - users collection, profile picture storage and user auth are set up and working in firebase

--- Care4U/Care4U/Care4UApp.swift
+++ Care4U/Care4U/Care4UApp.swift
@@ -14,30 +14,18 @@ import FirebaseCore
 @main
 struct Care4UApp: App {
     
+    @StateObject var authViewModel = AuthViewModel()
+    
     init() {
         FirebaseConfiguration.shared.setLoggerLevel(.min)
         FirebaseApp.configure()
         Auth.auth().signInAnonymously()
     }
-    
-    
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
 
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(authViewModel)
         }
-        .modelContainer(sharedModelContainer)
     }
 }
