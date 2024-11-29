7889b1104184b506d6a66008284154e28c73807c
Lisis <laruschel@proton.me>
2024-11-29 13:12:31 +0100

    changes app icon and name added splashscreen

--- Care4U/GoGig.swift
+++ Care4U/GoGig.swift
@@ -13,25 +13,22 @@ import GooglePlaces
 
 @main
 struct GoGig: App {
-    
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate     // initializing google place api
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     @StateObject var authViewModel = AuthViewModel()
     
     init() {
         FirebaseConfiguration.shared.setLoggerLevel(.min)
         FirebaseApp.configure()
-
     }
 
     var body: some Scene {
         WindowGroup {
-                
-                ContentView()
-                    .environmentObject(authViewModel)
-                    .applyBackground()
-            }
+            SplashScreen()
+                .environmentObject(authViewModel)
+                .applyBackground()
         }
     }
+}
     
 
 
