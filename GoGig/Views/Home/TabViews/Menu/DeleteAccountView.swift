a03202c4c0180883c9643122fda127e5d158910f
Lisis <laruschel@proton.me>
2024-12-08 18:03:10 +0100

    added pictures to readme fixed badge bug added AddrandomPosts function

---
+++ GoGig/Views/Home/TabViews/Menu/DeleteAccountView.swift
@@ -0,0 +1,18 @@
+//
+//  DeleteAccountView.swift
+//  GoGig 
+//
+//  Created by Lisis Ruschel on 06.12.24.
+//
+
+import SwiftUI
+
+struct DeleteAccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeleteAccountView()
+}
